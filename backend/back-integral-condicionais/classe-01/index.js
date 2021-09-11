e4ae13a1d4e21444345a50fdb3ed7e46d58fb84f
Jasmini Rebecca Gomes dos Santos <68917795+JasminiSantos@users.noreply.github.com>
2021-09-11 19:54:25 -0300

    Subindo condicionais

---
+++ backend/back-integral-condicionais/classe-01/index.js
@@ -0,0 +1,15 @@
+const jogada1 = "papel";
+const jogada2 = "tesoura";
+
+if((jogada1 == "pedra" && jogada2 =="tesoura")||(jogada2 == "pedra" && jogada1 =="tesoura")){
+    console.log("pedra");
+}
+else if((jogada1 =="papel" && jogada2 == "tesoura")||(jogada2 =="papel" && jogada1 == "tesoura")){
+    console.log("tesoura");
+}
+else if((jogada1 =="papel" && jogada2 == "pedra")||(jogada2 =="papel" && jogada1 == "pedra")){
+    console.log("papel");
+}
+else if(jogada1 == jogada2){
+    console.log("empate");
+}
